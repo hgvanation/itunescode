6b46f708c7597578a9bdb85be31d6f5ca1a24053
hgvanation <23001571@hus.edu.vn>
2026-08-06 16:06:25 +0700

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -128,10 +128,9 @@ function HomePage({ onAdminLogin }: { onAdminLogin: () => void }) {
     <main 
   className="w-full min-h-screen relative flex flex-col justify-between text-white bg-black bg-cover bg-center bg-no-repeat"
   style={{
-    backgroundImage: "url('/my-bg.jpg')"
+    backgroundImage: `url('/my-bg.jpg')`
   }}
 >
-
           {/* Nút Bỏ qua */}
           <button
             onClick={() => setShowSplash(false)}
