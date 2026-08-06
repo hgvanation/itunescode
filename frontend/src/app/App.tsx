c0fbdeee2d1407621a49786f0279c192748efead
hgvanation <23001571@hus.edu.vn>
2026-08-06 17:16:17 +0700

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -161,7 +161,7 @@ function HomePage({ onAdminLogin }: { onAdminLogin: () => void }) {
   className="w-full min-h-screen relative flex flex-col justify-between text-white bg-black bg-no-repeat bg-cover"
   style={{
     backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.55) 0%, rgba(0, 0, 0, 0.45) 100%), url('/my-bg.jpg')`,
-    backgroundPosition: `center bottom`
+    backgroundPosition: `center 10%`
   }}
 >
       {/* ── MÀN HÌNH INTRO VIDEO (5 GIÂY) ────────────────────────── */}
