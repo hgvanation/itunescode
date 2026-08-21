2d8c8e44804c9735ebd22fa1a27de135769fba0d
hgvanation <23001571@hus.edu.vn>
2026-08-21 20:49:49 +0700

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -235,7 +235,7 @@ function HomePage({ onAdminLogin }: { onAdminLogin: () => void }) {
           💌 Nhập địa chỉ email hoặc Threads ID để nhận code iTunes và cùng nhau tạo nên món quà ý nghĩa dành tặng Leo!
         </p>
         <p className="text-sm sm:text-lg text-slate-200 text-center mb-5 max-w-md drop-shadow font-serif italic tracking-widest">
-            𝓛𝓞𝓥𝓔 𝓛𝓔𝓞, 𝓣𝓔𝓝𝓓𝓔𝓡𝓛𝓨 ♡ 🎂🩵
+            𝓛𝓞𝓥𝓔 𝓛𝓔𝓞 𝓣𝓔𝓝𝓓𝓔𝓡𝓛𝓨 ♡ 🎂🩵
         </p>
 
         {/* Form Nhận Code */}
